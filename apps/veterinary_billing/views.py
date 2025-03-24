06360c92932cce710119c2226be709c7fe0b8f9c
oscar <ofacruz1@gmail.com>
2025-03-24 13:58:32 -0500

    Se crea html, css, urls

--- apps/veterinary_billing/views.py
+++ apps/veterinary_billing/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
 
 def billing_home(request):
-    return HttpResponse("¡Veterinary Billing está funcionando!")
+    return render (request,'veterinary_billing/home_billing.html')
--- apps/veterinary_home/urls.py
+++ apps/veterinary_home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('pets/', include('apps.veterinary_pets.urls')),
+    path('billing/', include('apps.veterinary_billing.urls')),
 ]
