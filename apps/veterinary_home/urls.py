67d8f3a065051bca180b9cf296fd465ef358b427
DeveloperMateo <mateo212705@gmail.com>
2025-03-29 16:16:39 -0500

    Aplication inventory update

--- apps/veterinary_home/urls.py
+++ apps/veterinary_home/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('pets/', include('apps.veterinary_pets.urls')),
     path('users/', include('apps.veterinary_users.urls')),
+    path('clinic/', include('apps.veterinary_clinic.urls')),
+    path('billing/', include('apps.veterinary_billing.urls')),
+    path('inventory/', include('apps.veterinary_inventory.urls')),
 ]
